1f9bc5a0e72b18f0347a21a2c44c80809010d184
cleitonrs <rscleitonrs@gmail.com>
2022-06-26 09:08:47 -0300

    responsive subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -37,19 +37,19 @@ const Subscribe = () => {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-xs md:max-w-[1100px] flex flex-col md:flex-row items-center justify-center md:justify-between mt-20 mx-auto">
+        <div className="max-w-xs md:max-w-[640px] text-center md:text-left">
           <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight text-black dark:text-gray-100">
-            Construa uma <strong className="text-cyan-400 dark:text-blue-500">aplicação completa</strong> do zero com <strong className="text-cyan-400 dark:text-blue-500">React</strong>
+          <h1 className="mt-8 text-[2.2rem] md:text-[] leading-tight text-black dark:text-gray-100">
+            Construa uma <strong className="text-cyan-400 dark:text-blue-500">aplicação completa,</strong> do zero, com <strong className="text-cyan-400 dark:text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-black dark:text-gray-200 leading-relaxed">
+          <p className="m-5 md:m-0 md:mt-4 text-black dark:text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
         <div className="p-8 bg-gray-200 dark:bg-gray-700 border border-gray-700 dark:border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">
+          <strong className="text-xl md:text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
