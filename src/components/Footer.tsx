ad9b171598b6853e15bc27c019bc3482610c51ad
cleitonrs <rscleitonrs@gmail.com>
2022-06-30 11:39:32 -0300

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Logo2 from './Logo2'
 const Footer = () => {
   return (
     <footer className="w-full px-4 md:py-5 flex flex-col md:flex-row items-center md:justify-between justify-center bg-gray-500 dark:bg-gray-900 md:border-t dark:border-gray-600 text-zinc-400">
-      <span className="w-96 md:w-0 h-[1px] md:h-0 dark:bg-gray-600"></span>
+      <span className="w-96 md:w-0 h-[1px] md:h-0 md:hidden dark:bg-gray-600"></span>
       <div className="flex flex-col md:flex-row items-center justify-center gap-6">
         <Logo2 />
         <span>
